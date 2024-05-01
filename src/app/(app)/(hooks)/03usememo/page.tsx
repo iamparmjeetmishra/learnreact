0154f55f7467bb558bbdb93f69ff1f49d200ff47
Parmjeet <iamparmjeetmishra@gmail.com>
2024-05-01 23:26:02 +0530

    LearnReact->03UseMemo with basic example

--- src/app/(app)/(hooks)/03usememo/page.tsx
+++ src/app/(app)/(hooks)/03usememo/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { initialItems } from "@/lib/utils";
 
 
@@ -9,9 +9,15 @@ interface DemoProps {}
 export default function Page({}: DemoProps) {
    
    const [count, setCount] = useState(0)
-   const [items] = useState(299999)
-
-   const selectedItem = items.find((item) => items.isSelected);
+   const [items] = useState(initialItems)
+	console.log('Log', count)
+	// This SelectedItems has performance issue
+   // const selectedItem = items.find((item) => item.isSelected);
+   
+	const selectedItem = useMemo(
+		() => items.find((item) => item.isSelected),
+		[items]
+	);
    
    return (
 		<main className="flex flex-col items-center justify-center min-h-screen">
@@ -22,8 +28,14 @@ export default function Page({}: DemoProps) {
 				<Button onClick={() => setCount(count + 1)}>Increment</Button>
 				<Button onClick={() => setCount(count - 1)}>Decrement</Button>
 			</div>
-			<div className="mt-4 border p-4">
-				<p>UseMemo</p>
+			<div className="mt-6 border p-4">
+				<p>In This below example without setting this large value in Memo- this method re-render everytime and find this large number. or unnecessary computation  </p>
+				<pre>
+					<code className="bg-gray-300">
+						`const selectedItem = items.find((item) => item.isSelected);`
+					</code>
+				</pre>
+				<p>If the item is not changing then value should not re-compute. It render only once and when the item is changed.</p>
 			</div>
 		</main>
 	);
