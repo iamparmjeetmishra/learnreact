354809f3887240895add1b3acacbcd847ebb05f0
Parmjeet <iamparmjeetmishra@gmail.com>
2024-05-03 20:46:41 +0530

    LearnReact->UI->01AnimatedBorderSpin

---
+++ src/app/(app)/(ui)/01animatedborder/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function AnimatedBorder() {
+	return (
+		<main className="bg-slate-950 flex flex-col min-h-screen items-center justify-center text-white">
+         <div className="card-wrapper size-52">
+            <div className="card-content flex items-center justify-center text-">Animated Border Spin</div>
+         </div>
+		</main>
+	);
+}
