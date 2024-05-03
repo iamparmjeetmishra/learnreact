c38b0ed033c854ec49553a177f0ceda9a38bb373
Parmjeet <iamparmjeetmishra@gmail.com>
2024-05-03 13:07:47 +0530

    LearnReact->Features->SearchTab UI Improved But Pending Axios

--- src/app/(app)/(features)/01searchbar/page.jsx
+++ src/app/(app)/(features)/01searchbar/page.jsx
@@ -7,18 +7,18 @@ export default function SearchBar() {
 
 	const [input, setInput] = useState('')
 	const [results, setResults] = useState([])
-	
+
 	const fetchData = (value) => {
 		const response = fetch("https://jsonplaceholder.typicode.com/users")
 			.then((response) => response.json())
 			.then((json) => {
 				// console.log(json)
 				const results = json.filter((user) => {
-					return value &&  user && user.name && user.name.toLowerCase().includes(value)
+					return value && user && user.name && user.name.toLowerCase().includes(value)
 				})
 				setResults(results)
 				console.log(results)
-		})
+			})
 	}
 
 	const handleChange = (value) => {
@@ -29,26 +29,34 @@ export default function SearchBar() {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen">
 			<div className="w-1/3 space-y-4">
-			<div className="flex gap-4 ">
-				<Input
-					className="border-2"
-					value={input}
-					onChange={(e) => handleChange(e.target.value)}
-               placeholder="Type to Search.."
-            />
-				<Button>Search</Button>
-			</div>
-			<div className="shadow-md border rounded p-4">
-				<ul>
+				<div className="flex gap-4 ">
+					<Input
+						className="border-2"
+						value={input}
+						onChange={(e) => handleChange(e.target.value)}
+						placeholder="Type to Search.."
+					/>
+					<Button>Search</Button>
+				</div>
 				{
 					results?.length > 0
-					? "No Result"
-					: results.map((result, id) => (
-						<li key={id}>{result.name}</li>
-					))
+						? <div className="shadow-md border rounded p-4">
+							<ul>
+								{
+									results?.length > 0
+										? results.map((result, id) => (
+											// <li key={id}>{result.name}</li>
+											<li key={id}>{result?.length > 0
+												? 'No result'
+												: result.name
+											}</li>
+										))
+										: "No Result"
+								}
+							</ul>
+						</div>
+						: ''
 				}
-				</ul>
-			</div>
 			</div>
 		</div>
 	);
