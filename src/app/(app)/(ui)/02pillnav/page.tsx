eb7056a7ef55f8ba431e987592666a8ca6b0b5d0
Parmjeet <iamparmjeetmishra@gmail.com>
2024-05-28 23:48:40 +0530

    LearnReact->UI-02PillNav using Framer Motion

--- src/app/(app)/(ui)/02pillnav/page.tsx
+++ src/app/(app)/(ui)/02pillnav/page.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client'
+import React, { useRef, useState } from 'react'
+import {motion} from 'framer-motion'
 
 export default function PillNav() {
   return (
@@ -10,19 +12,64 @@ export default function PillNav() {
 
 
 const SlideTabs = () => {
+
+   const [position, setPosition] = useState({
+      left: 0,
+      width: 0,
+      opacity: 0,
+   })
+
    return (
-      <ul className='relative mx-auto flex w-fit rounded-full border-2 border black bg-white p-1'>
-         <Tab>Home</Tab>
-         <Tab>Pricing</Tab>
-         <Tab>Features</Tab>
-         <Tab>Docs</Tab>
-         <Tab>Blog</Tab>
+      <ul
+         onMouseLeave={() => {
+            setPosition((pv) => ({
+               ...pv,
+               opacity: 0
+            }))
+         }}
+         className='relative mx-auto flex w-fit rounded-full border-2  border-black bg-white p-1'>
+         <Tab setPosition={setPosition}>Home</Tab>
+         <Tab setPosition={setPosition}>Pricing</Tab>
+         <Tab setPosition={setPosition}>Features</Tab>
+         <Tab setPosition={setPosition}>Docs</Tab>
+         <Tab setPosition={setPosition}>Blog</Tab>
+         <Cursor position={position} />
       </ul>
    )
 }
 
-const Tab = ({ children }) => {
+const Tab = ({ children, setPosition }) => {
+   const ref = useRef(null)
+
    return (
-      <li className='relative z-10 block cursor-pointer px-3 py-1.5 text-md uppercase text-white mix-blend-difference md:px-5 md:py-3'>{ children}</li>
+      <li
+         ref={ref}
+         onMouseEnter={() => {
+            if (!ref?.current) return;
+
+            // const data = ref.current.getBoundingClientRect()
+            // console.log(data)
+            
+            const {width} = ref.current.getBoundingClientRect()
+            // console.log(width)
+            setPosition({
+               left: ref.current.offsetLeft,
+               width,
+               opacity: 1,
+            })
+         }}
+         className='relative z-10 block cursor-pointer px-3 py-1.5 text-md uppercase text-white font-semibold mix-blend-difference md:px-5 md:py-3'
+      >
+         {children}
+      </li>
+   )
+}
+
+const Cursor = ({position}) => {
+   return (
+      <motion.li
+      animate={{...position}}
+      className='absolute z-0 h-7 rounded-full bg-black md:h-12'
+   />
    )
 }
