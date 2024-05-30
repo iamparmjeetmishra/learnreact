8135ff6cda9b496526408952ccce70c3dc403d73
Parmjeet <iamparmjeetmishra@gmail.com>
2024-05-30 23:18:00 +0530

    LearnReact->State Management-Redux-Counter

--- src/app/(app)/(stateManagement)/01redux/page.tsx
+++ src/app/(app)/(stateManagement)/01redux/page.tsx
@@ -7,15 +7,16 @@ import { Button } from "@/components/ui/button"
 
 
 export default function Redux() {
-   const count = useSelector((state:RootState) => state.counter.value)
+   const count = useSelector((state: RootState) => state.counter.value)
+   const dispatch = useDispatch()
   return (
      <div className="flex flex-col items-center justify-center min-h-screen">
         <h2 className="font-semibold text-2xl">Redux</h2>
         <p className="text-xl">count: {count}</p>
         <div className="flex gap-1 mt-4">
-           <Button>Increment</Button>
-           <Button>Decrement</Button>
-           <Button>IncrementByAmount</Button>
+           <Button onClick={() => dispatch(increment())}>Increment</Button>
+           <Button onClick={() => dispatch(decrement())} >Decrement</Button>
+           <Button onClick={() => dispatch(incrementByAmount(2))}>IncrementByAmount by 2</Button>
         </div>
     </div>
   )
