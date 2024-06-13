d365a0e27cef82204eac857c2c9268ac4ec9b6ce
Parmjeet <iamparmjeetmishra@gmail.com>
2024-06-13 23:02:20 +0530

    LearnReact->UIs->05 Collapsible SideNav Added and Working except TS error

--- src/app/(app)/(ui)/05collapsiblesidenav/page.tsx
+++ src/app/(app)/(ui)/05collapsiblesidenav/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header'
+import SideNav from './SideNav'
 
 export default function Collapsiblesidenav() {
   return (
      <div>
-        <Header />
-        05collapsiblesidenav</div>
+      <Header />
+      <SideNav />
+      </div>
   )
 }
