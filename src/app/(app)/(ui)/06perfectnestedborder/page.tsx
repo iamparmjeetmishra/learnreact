e8c15dc4acde2c3b92378c09ee6e5a5cdcb3230d
Parmjeet <iamparmjeetmishra@gmail.com>
2024-06-20 23:37:13 +0530

    LearnReact-UIs->06PerfectNestedBorder - code refactor

--- src/app/(app)/(ui)/06perfectnestedborder/page.tsx
+++ src/app/(app)/(ui)/06perfectnestedborder/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function PerfectNestedBorder() {
-  return (
-   <div className="w-full h-screen flex flex-col items-center justify-center bg-blue-950">
-   <div className="size-40 [--gap:16px] [--radius:30px] p-[--gap] rounded-[--radius] border-2 border-blue-600">
-     <div className="h-full rounded-[calc(var(--radius)-var(--gap))] border-2 border-yellow-500"></div>
-   </div>
- </div>
-  )
+	return (
+		<div className="w-full h-screen flex flex-col items-center justify-center bg-blue-950">
+			<div className="size-40 [--gap:16px] [--radius:30px] p-[--gap] rounded-[--radius] border-2 border-blue-600">
+				<div className="h-full rounded-[calc(var(--radius)-var(--gap))] border-2 border-yellow-500"></div>
+			</div>
+		</div>
+	);
 }
