79cbd8e8a8486e8e608e1d13f1ea21e7c3982e08
Parmjeet <iamparmjeetmishra@gmail.com>
2024-07-16 19:30:46 +0530

    Updated Tw config for support of js and jsx

--- src/app/(features)/03searchfilters/page.jsx
+++ src/app/(features)/03searchfilters/page.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+
+const options = [
+   { value: 'chocolate', label: 'Chocolate' },
+   { value: "vanilla", label: 'Vanilla' },
+   { value: 'strawberry', lable: 'Strawberry'}
+]
+
 
 export default function SearchFilters() {
   return (
-    <div>SearchFilters</div>
+     <div>
+        <h2 className='font-2xl bg-red-400'>Search Filters</h2>
+    </div>
   )
 }
