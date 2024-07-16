2cc692b0da915b463b62aa8395d67bc8e30e5111
Parmjeet <iamparmjeetmishra@gmail.com>
2024-07-16 20:35:35 +0530

    LearnReact->Features->03SearchFilters Finish

--- src/app/(features)/03searchfilters/page.jsx
+++ src/app/(features)/03searchfilters/page.jsx
@@ -1,15 +1,35 @@
+'use client'
+import { useState } from 'react'
+import Select from 'react-select'
 
 const options = [
    { value: 'chocolate', label: 'Chocolate' },
    { value: "vanilla", label: 'Vanilla' },
-   { value: 'strawberry', lable: 'Strawberry'}
+   { value: "strawberry", label: 'Strawberry' }
 ]
 
 
 export default function SearchFilters() {
-  return (
-     <div>
-        <h2 className='font-2xl bg-red-400'>Search Filters</h2>
-    </div>
-  )
+   return (
+      <div className="h-screen flex flex-col gap-4 items-center justify-center">
+         <h2 className='text-3xl font-bold'>Search Filters</h2>
+         <SearchFilter />
+      </div>
+   )
+}
+
+
+function SearchFilter() {
+   const [isSearchable, setIsSearchable] = useState('')
+   const [optionPicked, setOptionPicked] = useState('')
+   return (
+      <div className="min-w-96 border p-4">
+         <Select
+            id='Select'
+            options={options}
+            onChange={(option) => setOptionPicked(option)}
+         />
+         <h2>Option: {optionPicked.label}</h2>
+      </div>
+   )
 }
