e6a8b6f8c58fed751ddc3c2e5d389dfcbf5cc360
Parmjeet <iamparmjeetmishra@gmail.com>
2024-12-03 15:21:52 +0530

    Using column css line

---
+++ src/app/(ui)/07perfectimggallery/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+const ImgBucket = [
+  "https://images.unsplash.com/photo-1732919560838-a3209d3b131a?q=80&w=1504&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1731188678306-a2a6f9f3540b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1671490290845-f5edce3e1f74?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8",
+  "https://images.unsplash.com/photo-1718318102523-88b87d9238db?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1725995614860-0746f6d7cf35?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1733049149624-f90ec15fe533?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1717163050109-94d01be1b455?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1717423324236-ad5c8b826ecb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEzfHx8ZW58MHx8fHx8",
+  "https://images.unsplash.com/photo-1732705412353-a17b3c6babc8?q=80&w=1601&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+]
+
+export default function PerfectImgGalleryPage() {
+  return (
+    <div className='mx-auto bg-slate-100'>
+      <section className='container [columns:300px_4]'>
+        {ImgBucket.map((img, index) => (
+          <img key={index} src={img} alt="" className='mb-4' />
+        ))}
+      </section>
+    </div>
+  )
+}
