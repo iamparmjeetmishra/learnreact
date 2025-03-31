22f5c3af0f093651682f70c7677cd71ae674273f
parm <iamparmjeetmishra@gmail.com>
2025-03-31 22:26:19 +0530

    Motion 102 init

---
+++ src/app/(animation)/07motion102/card.tsx
@@ -0,0 +1,10 @@
+
+
+export const Card= () => {
+  return (
+    <div
+     className="w-72 min-h-[26rem]"
+    >card</div>
+  )
+}
+
---
+++ src/app/(animation)/07motion102/page.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils"
+import { Card } from "./card"
+import { GeistSans } from 'geist/font/sans';
+
+
+export const Content = () => {
+  return (
+    <>
+      <h2 className="text-center p-2 text-2xl font-semibold">Motion 102</h2>
+    <div
+      className={
+        cn(GeistSans.className,
+          "h-screen flex items-center justify-center bg-gray-50"
+        )
+      }
+      >
+      <Card />
+    </div>
+      </>
+  )
+}
+
+export default Content
