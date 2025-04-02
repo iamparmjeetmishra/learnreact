18c31c76f70cd199d1b618914cc34ba22bc8b93a
parm <iamparmjeetmishra@gmail.com>
2025-04-03 00:11:08 +0530

    Finish motion104

--- src/app/(animation)/09motion104/motion-hooks.tsx
+++ src/app/(animation)/09motion104/motion-hooks.tsx
@@ -1,27 +1,83 @@
 "use client";
 
 import { IconRocket } from "@tabler/icons-react";
+import { motion, useMotionTemplate, useMotionValueEvent, useScroll, useSpring, useTransform } from "motion/react";
 import Image from "next/image";
+import { useRef, useState } from "react";
 
 export default function MotionHooksExample() {
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const backgrounds = ["#343434", "#00193b", "#05291c"]
+
+  const [background, setBackground] = useState(backgrounds[0])
+
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start end", "end start"] // element = start, viewport = end
+  })
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    const finalValue = Math.floor(latest * backgrounds.length)
+    setBackground(backgrounds[finalValue])
+  }) 
 	return (
-		<div className="min-h-screen flex items-center justify-center bg-neutral-900">
+    <motion.div
+      ref={containerRef}
+      className="min-h-screen flex items-center justify-center bg-neutral-900"
+      animate={{
+        background
+      }}
+      transition={{
+        duration: 0.500,
+        ease: "easeInOut"
+      }}
+    >
 			<div className="mx-auto flex max-w-4xl flex-col gap-10 py-40">
 				{features.map((feature, idx) => (
 					<Card key={feature.title} feature={feature} />
 				))}
 			</div>
-		</div>
+		</motion.div>
 	);
 }
 
 const Card = ({ feature }: { feature: FeaturesType }) => {
+
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"] // element = start, viewport = end
+  })
+
+  const translateContent = useSpring(useTransform(scrollYProgress, [0, 1], [250, -250]), {
+    stiffness: 100,
+    damping: 20,
+    mass: 1,
+  })
+  const opacityTranform = useTransform(scrollYProgress, [0, 0.5, 1], [0, 1, 0])
+  const blurTransform = useTransform(scrollYProgress, [0.5, 1], [0, 10])
+  const scaleTransform = useTransform(scrollYProgress, [0.5, 1], [1, 0.8])
+
+  // useMotionValueEvent(scrollYProgress, "change", (latest) => {
+  //   console.log("change", latest);
+  // })
+
+ 
+
 	return (
-		<div
+    <div
+      ref={ref}
 			key={feature.title}
 			className="grid grid-cols-2 items-center gap-20 py-40"
 		>
-			<div className="flex flex-col gap-5">
+      <motion.div
+        style={{
+          // filter: `blur(${blurTransform}px)`, // Won't work
+          filter: useMotionTemplate`blur(${blurTransform}px)`,
+          scale: scaleTransform,
+        }}
+        className="flex flex-col gap-5">
 				{feature.icon}
 				<h2 className="text-4xl font-semibold text-white">
 					{feature.title}
@@ -29,8 +85,15 @@ const Card = ({ feature }: { feature: FeaturesType }) => {
 				<p className="text-lg text-neutral-400">
 					{feature.description}
 				</p>
-			</div>
-			<div>{feature.content}</div>
+			</motion.div>
+      <motion.div
+        style={{
+          y: translateContent,
+          opacity: opacityTranform,
+        }}
+      >
+        {feature.content}
+      </motion.div>
 		</div>
 	);
 };
