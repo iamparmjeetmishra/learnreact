a90eadf66946c629ceb9c5ada997f875b1972497
parm <iamparmjeetmishra@gmail.com>
2025-04-27 23:33:40 +0530

    data for picsum images

---
+++ src/lib/data.ts
@@ -0,0 +1,10 @@
+
+
+export const dynamicImages = [
+  "https://picsum.photos/id/1/500",
+  "https://picsum.photos/id/2/500",
+  "https://picsum.photos/id/3/500",
+  "https://picsum.photos/id/4/500",
+  "https://picsum.photos/id/5/500",
+  "https://picsum.photos/id/6/500",
+]
